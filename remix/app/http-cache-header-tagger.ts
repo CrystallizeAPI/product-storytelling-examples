41fc1c0151bb56348ba24c9d9a9cfc185e039f6e
Morel Sébastien <morel.seb@gmail.com>
2022-02-25 17:17:39 -0800

    :sparkles: :zap: Add HTTP Cache Headers

---
+++ remix/app/http-cache-header-tagger.ts
@@ -0,0 +1,26 @@
+import { Request } from 'remix';
+import parse from 'parse-duration';
+
+export type HttpCacheHeaders = {
+    headers: {
+        'Cache-Control': string;
+        'xkey': string; // Varnish cache key
+        'Surrogate-Key': string; // Fastly cache key
+    }
+};
+// Note: in a real project you only want one header depending you if work with Varnish or Fastly.
+// Todo: Manage this via ENV variables
+
+
+/**
+ * Return HTTP Cache headers
+ */
+export function HttpCacheHeaderTagger(maxAge: string, sharedMaxAge: string, tags: string[]): HttpCacheHeaders {
+    return {
+        headers: {
+            'Cache-Control': `public, max-age=${parse(maxAge, 's')}, s-maxage=${parse(sharedMaxAge, 's')}`,
+            'xkey': tags.join(','),
+            'Surrogate-Key': tags.join(',')
+        }
+    }
+}
--- remix/app/routes/index.tsx
+++ remix/app/routes/index.tsx
@@ -15,6 +15,7 @@ import Grid from "@crystallize/grid-renderer/react";
 import { GridItem } from "../components/grid-item";
 import { Products } from "../components/products";
 import { componentContent } from "../crystallize/utils/componentContent";
+import { HttpCacheHeaderTagger } from "~/http-cache-header-tagger";
 
 export let loader: LoaderFunction = async () => {
   const path = "/frontpage";
@@ -23,7 +24,7 @@ export let loader: LoaderFunction = async () => {
     { path }
   );
 
-  return json({ ...data, path });
+  return json({ ...data, path }, HttpCacheHeaderTagger('30s', '1w', ['frontpage']));
 };
 
 export let meta: MetaFunction = ({ data }) => {
@@ -43,6 +44,10 @@ export let meta: MetaFunction = ({ data }) => {
     }`,
   };
 };
+
+export function headers() {
+  return HttpCacheHeaderTagger('1m', '1w', ['index']).headers;
+}
 
 export default function Index() {
   let { catalogue, donuts } = useLoaderData();
