a91d21d928aba6f7d3a6b9d575ac52cabf443175
Morel Sébastien <morel.seb@gmail.com>
2022-03-02 13:55:15 -0800

    Change Purge to Purge Platform.sh Fastly Service

--- service-api/src/webhooks/item/published.js
+++ service-api/src/webhooks/item/published.js
@@ -1,28 +1,35 @@
+
+
+const FRONT_HOST = 'donutsh.milliseconds.live';
+
+function makePurgeRequest(host, path) {
+  return fetch(
+    `https://${host}${path}`,
+    {
+      method: "PURGE",
+      headers: {
+        'Fastly-Soft-Purge': '1'
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((json) => console.log(json))
+    .catch((err) => console.error("error:" + err));
+};
+
 module.exports = async function itemPublished(payload) {
   console.log("Webhook payload: itemPublished");
   console.log(JSON.stringify(payload, null, 2));
-
-  let purgePath = payload.item.get.tree.path;
-  let paths = ["/", purgePath];
+  const purgePath = payload.item.get.tree.path;
   console.log(purgePath);
+
+  let purgeRequests = [];
+  purgeRequests.push(makePurgeRequest(FRONT_HOST, "/"));
+  purgeRequests.push(makePurgeRequest(FRONT_HOST, purgePath));
+
   let relatingItems = payload.item.get?.relatingItems;
-
   for (let item in relatingItems) {
-    paths.push(relatingItems[item].tree?.path);
+    purgeRequests.push(makePurgeRequest(FRONT_HOST, relatingItems[item].tree?.path));
   }
-
-  for (let path in paths) {
-    const purgeRequest = await fetch(
-      `https://dounot.milliseconds.live${paths[path]}`,
-      {
-        method: "PURGE",
-        headers: {
-          'Fastly-Soft-Purge': '1'
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((json) => console.log(json))
-      .catch((err) => console.error("error:" + err));
-  }
+  await Promise.all(purgeRequests);
 };
