b695c059e38076d31befbfc344b43ea26a40d881
Dhairya <dhairiya96@gmail.com>
2022-03-02 20:46:37 +0100

    Update published.js

--- service-api/src/webhooks/item/published.js
+++ service-api/src/webhooks/item/published.js
@@ -1,5 +1,3 @@
-const path = require("node:path/posix");
-
 module.exports = async function itemPublished(payload) {
   console.log("Webhook payload: itemPublished");
   console.log(JSON.stringify(payload, null, 2));
